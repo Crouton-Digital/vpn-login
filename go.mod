900df6581fe52913130e62ba73b37321abc89cdc
Rolands Kalpins <rolands.kalpins@gmail.com>
2022-08-09 12:53:15 +0300

    access.log all arguments

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bitbucket.org/am-bitbucket/vpn-login
+
+go 1.18
---
+++ login.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var arguments string
+	for _, arg := range os.Args[:] {
+		// fmt.Println(arg)
+		arguments += arg + " "
+	}
+
+	arguments += "\n"
+
+	if _, err := f.Write([]byte(arguments)); err != nil {
+		f.Close() // ignore error; Write error takes precedence
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(os.Stdout, "0")
+}
